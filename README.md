de5675e73f2813b1394904c782c72dae26d0e0b1
Hamdollah Hosseinpour <cn120.2010@gmail.com>
2023-01-31 23:49:09 +0330

    add this training

---
+++ README.md
@@ -0,0 +1,18 @@
+# Svelte Feedback App
+
+This is the project from the Svelte crash course on https://www.youtube.com/@TraversyMedia
+
+```bash
+
+# Install dependencies
+npm install
+
+# Dev server
+npm run dev (http://localhost:5000)
+
+# Prod server
+npm start
+
+# Build for production
+npm run build
+```
---
+++ src/components/FeedbackStats.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { FeedbackStore } from "../stores";
+
+    $: count = $FeedbackStore ? $FeedbackStore.length : 0;
+    $: average = $FeedbackStore && count ? $FeedbackStore.reduce((a, {rating}) =>
+        a + rating, 0) / count : 0;
+</script>
+
+<div class="feedback-stats">
+    <h4>{count} Reviews</h4>
+    <h4>Rating Average: {average}</h4>
+</div>
+
+<style>
+    .feedback-stats {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
